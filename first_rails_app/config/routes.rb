3c6f87e1aafc6acd5de0c436b24c613c417d3156
Seamoon Pandey <hackedasocial@gmail.com>
2023-06-23 17:04:18 +0545

    minor changes

--- first_rails_app/config/routes.rb
+++ first_rails_app/config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  get 'welcome/home','welcome#home'
+  root 'pages#home'
+  get 'about',to:'pages#about'
 end
