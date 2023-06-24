37e3650602b4144c1ffa39c94e5516457ada3e3e
Seamoon Pandey <hackedasocial@gmail.com>
2023-06-24 11:24:15 +0545

    destroy and partials, wrap

--- crud_app/app/controllers/articles_controller.rb
+++ crud_app/app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+    before_action :set_article, only:[:edit,:show,:destroy,:update]
+
     def index
         @articles = Article.all
     end
@@ -9,21 +11,21 @@ class ArticlesController < ApplicationController
     end
 
     def edit 
-        @article = Article.find(params[:id])
+       
     end
 
     def create
         @article = Article.new(article_params)
         if @article.save
             flash[:notice] = "Article was successfully created"
-            redirect_to articles_show(@article)
+            redirect_to article_path(@article)
         else
             render'new'
         end 
     end
 
     def update
-        @article = Article.find(params[:id])
+       
         if @article.update(article_params)
             flash[:notice]="Article was updated successfully"
             redirect_to article_path(@article)
@@ -33,10 +35,21 @@ class ArticlesController < ApplicationController
     end
 
     def show
-        @article = Article.find(params[:id])
+       
+    end
+
+    def destroy       
+        @article.destroy
+        flash[:notice] = "Article was succesfully deleted"
+        redirect_to articles_path
     end
 
     private
+
+        def set_article
+            @article = Article.find(params[:id])
+        end
+
         def article_params
             params.require(:article).permit(:title, :description)
         end
